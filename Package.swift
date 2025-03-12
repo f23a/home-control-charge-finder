db6110a9202b5fa761e497083c3a3c7f9b27e91f
Christoph Pageler <christoph.pageler@me.com>
2025-03-12 19:59:43 +0100

    Fix Kit

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     platforms: [.macOS(.v15)],
     dependencies: [
 //        .package(path: "../home-control-client"),
-        .package(path: "../home-control-kit"),
-        .package(url: "https://github.com/f23a/home-control-client.git", from: "1.7.3"),
+//        .package(path: "../home-control-kit"),
+        .package(url: "https://github.com/f23a/home-control-client.git", from: "1.7.4"),
         .package(url: "https://github.com/f23a/home-control-logging.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0")
     ],
